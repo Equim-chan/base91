886cfdb8b8d08eec331202fa77e6e17d16421c29
Equim <sayaka@ekyu.moe>
2017-05-26 03:49:00 +0800

    remove unrelated files

--- try.js
+++
@@ -1,4 +0,0 @@
-const base91 = require('.');
-
-const z91 = base91.decode('Jo9Kc)qCH$/xaxHl3o=Cq/6Y%y|u=EuiKBs3O:FZ<R4tM.kL5D^g66nNxS*/{B[p^iwJL,&elTR;axLmeBMf,W?NxS,/},kLxo>vm)UI97?+5E{ouA');
-console.log(z91.toString());  // =>
